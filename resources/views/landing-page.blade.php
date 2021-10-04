56574dd26172c4ff8ad3b12331fa45f2be4a39cd
unknown <lycatocmo57@gmail.com>
2021-10-04 22:48:50 +0800

    first commit

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,19 @@
+
+@extends('base')
+
+@section('content')
+
+<style>
+
+body{
+        background-image:url('welcome.gif');
+        background-size: cover;
+    }
+</style>
+
+<br>
+<body>
+   
+</body>
+
+
